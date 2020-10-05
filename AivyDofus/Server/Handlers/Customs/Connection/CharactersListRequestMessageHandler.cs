45ec5fb22948a1b4f53899357b4c7ecea58df0ba
Tsiry <rakotobetsiriniaina@gmail.com>
2020-10-06 01:00:05 +0200

    fix - creation

--- AivyDofus/Server/Handlers/Customs/Connection/CharactersListRequestMessageHandler.cs
+++ AivyDofus/Server/Handlers/Customs/Connection/CharactersListRequestMessageHandler.cs
@@ -29,23 +29,27 @@ namespace AivyDofus.Server.Handlers.Customs.Connection
 
         }
 
+        public static readonly NetworkElement _characters_list_message = BotofuProtocolManager.Protocol[ProtocolKeyEnum.Messages, x => x.name == "CharactersListMessage"];
+        public static NetworkContentElement _characters_list_content(IEnumerable<PlayerData> players, ServerData server_data, bool hasStartupActions = false)
+        {
+            return new NetworkContentElement()
+            {
+                fields =
+                {
+                    { "hasStartupActions", hasStartupActions },
+                    { "characters", players.Select(x => server_data.Type == 1 ? x.BaseHardcoreInformation() : x.BaseInformation()).ToArray() }
+                }
+            };
+        }
+
         public override void Handle()
         {
             DofusServerWorldClientReceiveCallback _world_callback = _casted_callback<DofusServerWorldClientReceiveCallback>();
             if (DofusServer._server_api.GetData(x => x.Port == _world_callback._server.Port) is ServerData server_data &&
                 DofusServer._server_api.GetData<PlayerData>(x => x.AccountToken == _world_callback._client.CurrentToken && x.ServerId == server_data.ServerId) is IEnumerable<PlayerData> players)
             {
-                NetworkElement characters_list_message = BotofuProtocolManager.Protocol[ProtocolKeyEnum.Messages, x => x.name == "CharactersListMessage"];
-                NetworkContentElement characters_list_content = new NetworkContentElement()
-                {
-                    fields =
-                    {
-                        { "hasStartupActions", false },
-                        { "characters", players.Select(x => server_data.Type == 1 ? x.BaseHardcoreInformation() : x.BaseInformation()).ToArray() }
-                    }
-                };
-
-                Send(false, _callback._client, characters_list_message, characters_list_content);
+                NetworkContentElement characters_list_content = _characters_list_content(players, server_data);
+                Send(false, _callback._client, _characters_list_message, characters_list_content);
             }
         }
 
--- AivyDofus/Server/Handlers/Customs/Creation/CharacterCreationRequestMessageHandler.cs
+++ AivyDofus/Server/Handlers/Customs/Creation/CharacterCreationRequestMessageHandler.cs
@@ -5,6 +5,7 @@ using AivyDofus.Extension.Server.Data;
 using AivyDofus.Handler;
 using AivyDofus.Protocol.Elements;
 using AivyDofus.Server.Callbacks;
+using AivyDofus.Server.Handlers.Customs.Connection;
 using AivyDomain.Callback.Client;
 using NLog;
 using System;
@@ -12,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AivyDofus.Server.Handlers.Customs.Creation
@@ -29,12 +31,34 @@ namespace AivyDofus.Server.Handlers.Customs.Creation
 
         }
 
+        private static readonly NetworkElement _character_creation_result = BotofuProtocolManager.Protocol[ProtocolKeyEnum.Messages, x => x.name == "CharacterCreationResultMessage"];
+        private static NetworkContentElement _character_creation_result_content(string error_code)
+        {
+            return new NetworkContentElement()
+            {
+                fields =
+                {
+                    { "result", int.Parse(BotofuProtocolManager.Protocol[x => x.name == "CharacterCreationResultEnum"][error_code]) }
+                }
+            };
+        }
+
         public override void Handle()
         {
             DofusServerWorldClientReceiveCallback _world_callback = _casted_callback<DofusServerWorldClientReceiveCallback>();
             if (DofusServer._server_api.GetData(x => x.Port == _world_callback._server.Port) is ServerData server_data)
             {
+                if (DofusServer._server_api.GetData<PlayerData>(x => x.AccountToken == _world_callback._client.CurrentToken).Count() >= 5)
+                    throw new CharacterCreationException("ERR_TOO_MANY_CHARACTERS");
+
                 string name = _content["name"];
+
+                if (!Regex.IsMatch(name, @"(\w){3,20}"))
+                    throw new CharacterCreationException("ERR_INVALID_NAME");
+
+                if (DofusServer._server_api.GetData<PlayerData>(x => x.Name.ToLower() == name.ToLower()).Count() > 0)
+                    throw new CharacterCreationException("ERR_NAME_ALREADY_EXISTS");
+
                 int[] colors = (_content["colors"] as dynamic[]).Select(x => x is int v ? v : -1).ToArray();
                 byte breed = _content["breed"];
                 bool sex = _content["sex"];
@@ -48,7 +72,7 @@ namespace AivyDofus.Server.Handlers.Customs.Creation
 
                 while(rnd_id == -1)
                 {
-                    rnd_id = rnd.Next(1, short.MaxValue);
+                    rnd_id = rnd.Next(1, int.MaxValue - 1);
                     if(DofusServer._server_api.GetData<PlayerData>(x => x.Id == rnd_id).Count() > 0)
                     {
                         rnd_id = -1;
@@ -64,16 +88,44 @@ namespace AivyDofus.Server.Handlers.Customs.Creation
                     Sex = sex,
                     ServerId = server_data.ServerId,
                     CreationDateTime = DateTime.Now,
-                    Look = breed_data.Build(head_data, colors, sex) // to do parse
+                    Look = breed_data.Build(head_data, colors, sex) 
                 };
 
                 DofusServer._server_api.UpdateData(_player);
             }
         }
 
+        // if no error
+        public override void EndHandle()
+        {
+            Send(false, _callback._client, _character_creation_result, _character_creation_result_content("OK"));
+            DofusServerWorldClientReceiveCallback _world_callback = _casted_callback<DofusServerWorldClientReceiveCallback>();
+            if (DofusServer._server_api.GetData(x => x.Port == _world_callback._server.Port) is ServerData server_data &&
+                DofusServer._server_api.GetData<PlayerData>(x => x.AccountToken == _world_callback._client.CurrentToken && x.ServerId == server_data.ServerId) is IEnumerable<PlayerData> players)
+            {
+                NetworkContentElement characters_list = CharactersListRequestMessageHandler._characters_list_content(players, server_data);
+                Send(false, _callback._client, CharactersListRequestMessageHandler._characters_list_message, characters_list);
+                // connect client
+            }
+        }
+
         public override void Error(Exception e)
         {
-            logger.Error(e);
+            if(e is CharacterCreationException result)
+            {
+                Send(false, _callback._client, _character_creation_result, _character_creation_result_content(result.error_code));
+            }
+            else logger.Error(e);
+        }
+
+        private class CharacterCreationException : Exception
+        {
+            public string error_code { get; set; }
+
+            public CharacterCreationException(string error_code)
+            {
+                this.error_code = error_code;
+            }
         }
     }
 }
